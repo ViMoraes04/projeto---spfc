7604287990cb943926602a592f3adf7227b0c777
vitoria <vitoria.moraes@sptech.school>
2024-11-12 15:54:19 -0300

    aplicando a web-data-viz

--- SQL/scriptProjetospfc.sql
+++ SQL/scriptProjetospfc.sql
@@ -9,6 +9,12 @@ posicao VARCHAR(20) NOT NULL,
 nmr_camisa INT NOT NULL
 );
 
+CREATE TABLE Titulos(
+idTitulos INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(45),
+qtdTitulos INT
+);
+
 
 CREATE TABLE Usuarios(
 idUsuario INT PRIMARY KEY AUTO_INCREMENT,
@@ -17,25 +23,11 @@ dtNasc DATE NOT NULL,
 email VARCHAR(80) NOT NULL,
 senha VARCHAR(45) NOT NULL,
 fkFavoritos INT,
-CONSTRAINT fkFavoritosUsuarios FOREIGN KEY(fkFavoritos) REFERENCES Jogadores(idJogadores)
+fkTituloFavorito INT,
+CONSTRAINT fkFavoritosUsuarios FOREIGN KEY(fkFavoritos) REFERENCES Jogadores(idJogadores),
+CONSTRAINT fkTituloFavorito_Usuario FOREIGN KEY(fkTituloFavorito) REFERENCES Titulos(idTitulos)
 );
 
-CREATE TABLE Partidas(
-idPartidas INT PRIMARY KEY AUTO_INCREMENT,
-qtdAno INT NOT NULL,
-qtdMes INT NOT NULL
-);
-
-CREATE TABLE Gols (
-idGols INT,
-qtdAno INT,
-qtdMes INT,
-fkJogadores INT,
-fkPartidas INT,
-CONSTRAINT pkComposta PRIMARY KEY (idGols, fkJogadores, fkPartidas),
-CONSTRAINT fkJogadoresGols FOREIGN KEY(fkJogadores) REFERENCES Jogadores(idJogadores),
-CONSTRAINT fkPartidasGols FOREIGN KEY(fkPartidas) REFERENCES Partidas(idPartidas)
-);
 
 INSERT INTO Jogadores VALUES
 (default, 'Rafael', 'Goleiro', 23),
@@ -50,3 +42,18 @@ INSERT INTO Jogadores VALUES
 (default, 'Luciano', 'Atacante', 10),
 (default, 'Calleri', 'Centroavante', 9);
 
+
+select * from Usuarios;
+
+INSERT INTO Usuarios (nome, email, senha, fkFavoritos, fkTitulos) VALUES (); 
+
+ALTER TABLE Usuarios 
+DROP COLUMN dtNasc;
+select * from Titulos;
+INSERT INTO Titulos VALUES
+(default, 'Copa Libertadores', 3),
+(default, 'Mundial de Clubes', 3),
+(default, 'Copa do Brasil', 1),
+(default, 'Copa Sul-Americana', 1),
+(default, 'Campeonato Paulista', 22),
+(default, 'Campeonato Brasileiro', 6);
